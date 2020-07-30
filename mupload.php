1d4c25e456931d6cd1732de9e16795968050fd81
Revan <43107289+revanrohith@users.noreply.github.com>
2020-07-30 13:27:30 +0530

    Delete mupload.php

--- mupload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$info = pathinfo($_FILES['upl']['name']);
-$ext = $info['extension'];
-$newname = "toClip.".$ext;
-$target = 'uploads/'.$newname;
-move_uploaded_file( $_FILES['upl']['tmp_name'], $target);
-sleep(3);
-header("Location: index.html");
-?>
